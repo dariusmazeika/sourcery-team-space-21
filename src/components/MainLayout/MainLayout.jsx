e91a61c1576b78d9d1d1e754a826db7dcd8cf6e4
Aivaras <as.savinis@gmail.com>
2021-11-07 12:30:40 +0200

    Added MainLayout component and story. Added some variables for rem units. Added variables for colors.
    In future tasks I will fix header and footer fonts.

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./_styles.module.scss";
+import PropTypes from "prop-types";
+import logoDark from "../../assets/logo-dark.png";
+
+export const MainLayout = (props) => {
+  const { children } = props;
+
+  return (
+    <div>
+      <header>
+        <div className={styles.navigationMeta}>
+          <div className={styles.navigationMetaLogo}>
+            <img src={logoDark} alt="logotype" />
+          </div>
+          <div className={styles.navigationMetaTeam}>
+            <div>Team</div>
+            <div>Inbif</div>
+          </div>
+        </div>
+        {/*<nav>{ navigation links }</nav>*/}
+        {/*<div>{ bell icon & user menu }</div>*/}
+      </header>
+
+      <main>{children}</main>
+
+      <footer>
+        <div>COPYRIGHT &copy; 2021 DEVBRIDGE</div>
+      </footer>
+    </div>
+  );
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.any,
+};
