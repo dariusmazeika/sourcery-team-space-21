9f5c3679bcdf32b7b8412b9ae38f5a242c7a9af9
rimkusaurimas <labas@aurimasrimkus.lt>
2021-11-10 23:57:31 +0200

    Babel env and react presets

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
-import { babel } from '@rollup/plugin-babel';
+import { babel } from "@rollup/plugin-babel";
 
 const config = {
-  input: 'src/components/icon/library/index.js',
+  input: "src/components/icon/library/index.js",
   output: {
-    file: 'src/components/icon/dist/index.esm.js',
-    format: 'esm'
+    file: "src/components/icon/dist/index.esm.js",
+    format: "esm",
   },
   external: [/@babel\/runtime/],
-  plugins: [babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] })]
+  plugins: [
+    babel({
+      babelHelpers: "runtime",
+      plugins: ["@babel/plugin-transform-runtime"],
+    }),
+  ],
 };
 
 export default config;
