02331b136368a3489fa785e2c101f9df50240c71
Marius <marius.budreika09@gmail.com>
2021-11-13 19:40:11 +0200

    Added index.js, fixed scss class naming

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { MainLayout } from "components/MainLayout";
+import { Button } from "components/button";
 
 function App() {
-  return <MainLayout className="app">{}</MainLayout>;
+  return (
+    <MainLayout className="app">
+      <Button type="primary">DEFAULT</Button>
+      {}
+    </MainLayout>
+  );
 }
 
 export default App;
