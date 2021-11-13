a9b2ed42d06ea0fbeac821e031c96e4763bbe70b
Marius <marius.budreika09@gmail.com>
2021-11-13 17:17:25 +0200

    merged individual button components into one component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,29 +1,47 @@
 import React from "react";
-import buttonSccs from "./button.module.scss";
 import PropTypes from "prop-types";
+import cx from "classnames";
 
-export const Button = ({ size, text }) => {
+import styles from "./button.module.scss";
+
+const Type = {
+  PRIMARY: "primary",
+  TEXT: "text",
+  ACTION: "action",
+};
+
+const Size = {
+  LARGE: "large",
+};
+
+const Color = {
+  SECONDARY: "secondary",
+  DUOCOLOR: "reverse",
+};
+
+export const Button1 = ({ children, type, size, color }) => {
   return (
     <button
-      className={
-        size === "Medium"
-          ? `${buttonSccs.Medium} ${buttonSccs.btn}`
-          : size === "Large"
-          ? `${buttonSccs.Large} ${buttonSccs.btn}`
-          : " "
-      }
+      className={cx(styles.btn, {
+        [styles.large]: size === Size.LARGE,
+        [styles.primary]: type === Type.PRIMARY,
+        [styles.text]: type === Type.TEXT,
+        [styles.action]: type === Type.ACTION,
+        [styles.secondary]: color === Color.SECONDARY,
+        [styles.reverse]: color === Color.DUOCOLOR,
+      })}
     >
-      {text}
+      {children}
     </button>
   );
 };
 
-Button.propTypes = {
-  text: PropTypes.string,
-  size: PropTypes.oneOf(["Medium", "Large"]).isRequired,
-};
-
-Button.defaultProps = {
-  text: "DEFAULT",
-  size: "Medium",
+Button1.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  type: PropTypes.oneOf(Object.values(Type)).isRequired,
+  size: PropTypes.oneOf(Object.values(Size)),
+  color: PropTypes.oneOf(Object.values(Color)),
 };
