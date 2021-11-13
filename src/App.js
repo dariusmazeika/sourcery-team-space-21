3c0cbe18ea164844ab6a8dc1a3cd99b76988b2bb
Marius <marius.budreika09@gmail.com>
2021-11-13 21:41:38 +0200

    scss fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { Button } from "components/button";
 function App() {
   return (
     <MainLayout className="app">
-      <Button type="primary">DEFAULT</Button>
+      <Button type="action" color="reverse">
+        X
+      </Button>
       {}
     </MainLayout>
   );
