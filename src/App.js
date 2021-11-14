f71bb51687312b7de129c593f156148910ef9c99
Marius <marius.budreika09@gmail.com>
2021-11-14 12:37:52 +0200

    button scss fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
 import { MainLayout } from "components/MainLayout";
-import { Button } from "components/button";
 
 function App() {
-  return (
-    <MainLayout className="app">
-      <Button type="action" color="reverse">
-        X
-      </Button>
-      {}
-    </MainLayout>
-  );
+  return <MainLayout className="app">{}</MainLayout>;
 }
 
 export default App;
