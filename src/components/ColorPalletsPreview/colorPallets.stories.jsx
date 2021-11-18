4eec8b382e0d831dd84f117bb6452d81c568e5ba
Imantas Cesliavicius <imantas.cesliavicius@gmail.com>
2021-11-18 20:11:29 +0200

    initial

---
+++ src/components/ColorPalletsPreview/colorPallets.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ColorPallets } from "./colorPallets";
+
+export default {
+  title: "Example/ColorPalletsPreview",
+  component: ColorPallets,
+};
+
+export const ColorPalletsPreview = () => <ColorPallets />;
