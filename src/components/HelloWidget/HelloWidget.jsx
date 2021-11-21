febb1dd9196b53dd26c732b69df33acd651dfd52
Marius <marius.budreika09@gmail.com>
2021-11-21 13:31:25 +0200

    initial hello widget with styles

---
+++ src/components/HelloWidget/HelloWidget.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import propTypes from "prop-types";
+import styles from "components/HelloWidget/hello-widget.module.scss";
+
+export const HelloWidget = ({ name = "Jonas" }) => {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(
+        date
+          .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+          .replace("AM", "")
+          .replace("PM", "")
+      );
+    }, 1000);
+  }, []);
+  return (
+    <>
+      <div className={styles.clock}>{clockState}</div>
+      <div className={styles.text}>Good afternoon, {name}!</div>
+    </>
+  );
+};
+
+HelloWidget.propTypes = {
+  name: propTypes.string,
+};
