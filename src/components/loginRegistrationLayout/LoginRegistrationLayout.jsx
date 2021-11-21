aaaeb71cfff5c498b7935a6cbcbb741d78e52c72
Darius M <darius.post@gmail.com>
2021-11-21 14:38:49 +0200

    added background and logo

--- src/components/loginRegistrationLayout/LoginRegistrationLayout.jsx
+++ src/components/loginRegistrationLayout/LoginRegistrationLayout.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import styles from "./loginRegistrationLayout.module.scss";
+import logoGradientBackground from "assets/logo-gradient-background.png";
 
-const LoginRegistrationLayout = ({ formTitle, formSubtitle }) => {
+const LoginRegistrationLayout = ({ formTitle, formSubtitle, children }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.containerHeader}>
-        <h2 className={cx(styles.containerHeaderTitle, "h2-alt")}>
-          {formTitle}
-        </h2>
-        <p className={styles.containerHeaderSubtitle}>{formSubtitle}</p>
+    <div className={styles.main}>
+      <div className={styles.mainBox}>
+        <div className={styles.logo}>
+          <img
+            src={logoGradientBackground}
+            alt="inbif team logotype"
+            className={styles.logoImg}
+          />
+          <div className={styles.logoText}>Team Inbif</div>
+        </div>
+        <div className={styles.formContainer}>
+          <div className={styles.formContainerHeader}>
+            <h2 className={cx(styles.formContainerHeaderTitle, "h2-alt")}>
+              {formTitle}
+            </h2>
+            <p className={styles.formContainerHeaderSubtitle}>{formSubtitle}</p>
+          </div>
+          <div className={styles.formContainerBody}>{children}</div>
+          <div className={styles.formContainerActionsBlock}></div>
+        </div>
       </div>
-      <div className={styles.containerBody}></div>
-      <div className={styles.containerFooter}></div>
     </div>
   );
 };
@@ -21,6 +34,7 @@ const LoginRegistrationLayout = ({ formTitle, formSubtitle }) => {
 LoginRegistrationLayout.propTypes = {
   formTitle: PropTypes.string,
   formSubtitle: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default LoginRegistrationLayout;
