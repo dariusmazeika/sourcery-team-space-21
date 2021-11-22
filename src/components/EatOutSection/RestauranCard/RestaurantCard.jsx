8c61e51284ceaf218cea16c37049ad17e4ffbae4
rimkusaurimas <labas@aurimasrimkus.lt>
2021-11-22 14:34:26 +0200

    E21T-65

--- src/components/EatOutSection/RestauranCard/RestaurantCard.jsx
+++ src/components/EatOutSection/RestauranCard/RestaurantCard.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./restaurant-card.module.scss";
 
+import placeholderPhoto from "assets/restaurant-card-placeholder.jpg";
+
 export const RestaurantCard = (props) => {
+  const roundIcon = <div className={styles.ellipse}></div>;
+
   return (
-    <section>
-      <p className={styles.test}>Test</p>
+    <section className={styles.card}>
+      <img
+        className={styles.placeholderPhoto}
+        src={placeholderPhoto}
+        alt="Restaurant Snapshot"
+      />
+      <div className={styles.checkIn}></div>
+      <div className={styles.rating}></div>
+      <div className={styles.details}>
+        <div className={styles.topTextWrapper}>
+          <p className={styles.topText}>salads</p>
+          {roundIcon}
+          <p className={styles.topText}>snacks</p>
+          {roundIcon}
+          <p className={styles.topText}>pizza</p>
+        </div>
+        <h3 className={styles.title}>Ristorante Viva Piccola Italia</h3>
+        <p className={styles.bottomText}>10:00 - 21:00</p>
+      </div>
+      <div className={styles.wishlist}></div>
     </section>
   );
 };
--- src/components/EatOutSection/RestauranCard/RestaurantCard.stories.jsx
+++ src/components/EatOutSection/RestauranCard/RestaurantCard.stories.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { RestaurantCard } from "./RestaurantCard";
+import { withKnobs } from "@storybook/addon-knobs";
 
 export default {
   title: "Components / RestaurantCard",
+  decorators: [withKnobs],
   component: RestaurantCard,
 };
 
