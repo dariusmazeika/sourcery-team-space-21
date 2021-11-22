26ed666bea803f728f0bf091f15798a79a0d80f7
Aidas <aidas19970711@gmail.com>
2021-11-22 19:15:24 +0200

    Remove DashboardLayout component

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { DashboardLayout } from "components/Dashboard/DashboardLayout";
 
 export const DashboardPage = () => {
-  return <DashboardLayout>{}</DashboardLayout>;
+  return <div>{}</div>;
 };
