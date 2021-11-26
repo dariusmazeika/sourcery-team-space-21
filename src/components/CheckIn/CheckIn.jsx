b16ed17645a66059ec4903735634382ecde53f1d
Marius <marius.budreika09@gmail.com>
2021-11-26 13:57:04 +0200

    added icon and style for component

--- src/components/CheckIn/CheckIn.jsx
+++ src/components/CheckIn/CheckIn.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+import SvgIconCheckInPerson from "components/icon/library/IconCheckInPerson";
 
 import styles from "./check-in.module.scss";
 
-export const CheckIn = () => {
+export const CheckIn = ({ count = "6" }) => {
   return (
     <div className={styles.wrapper}>
-      <div></div>
+      <SvgIconCheckInPerson size="1.2rem" />
+      <div className={styles.count}>{count}</div>
     </div>
   );
+};
+
+CheckIn.propTypes = {
+  count: PropTypes.number,
 };
