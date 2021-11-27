b67cffe82077617f87a9c0f3861e9476128c8c9c
Imantas Cesliavicius <imantas.cesliavicius@gmail.com>
2021-11-27 23:48:39 +0200

    -browse button text 1.2rem, updated button jsx and scss

---
+++ src/components/EatOutSection/BrowseCardComponent/BrowseCard.jsx
@@ -0,0 +1,18 @@
+import { Button } from "components/button/Button";
+import React from "react";
+import styles from "./browse-card.module.scss";
+
+export const BrowseCard = () => {
+  return (
+    <section className={styles.card}>
+      <h3 className={styles.cardHeading}>
+        View all your favourite lunch spots and more
+      </h3>
+      <Button size={"small"} kind={"primary"}>
+        browse list
+      </Button>
+    </section>
+  );
+};
+
+export default BrowseCard;
