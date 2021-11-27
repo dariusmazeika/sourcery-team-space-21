0218b9564c719aaa11a50f687ce824d179f0941c
rimkusaurimas <labas@aurimasrimkus.lt>
2021-11-27 17:56:39 +0200

    Some bug fixes from PR comments

--- src/components/EatOutSection/RestaurantCard/RestaurantCard.jsx
+++ src/components/EatOutSection/RestaurantCard/RestaurantCard.jsx
@@ -1,32 +1,65 @@
 import React from "react";
 import styles from "./restaurant-card.module.scss";
+import PropTypes from "prop-types";
 
-import placeholderPhoto from "assets/restaurant-card-placeholder.jpg";
-
-export const RestaurantCard = (props) => {
+const RestaurantCard = ({
+  restaurantPhoto,
+  cardTopTextCategory1,
+  cardTopTextCategory2,
+  cardTopTextCategory3,
+  cardTitle,
+  cardOpenTime,
+  cardClosingTime,
+}) => {
   const roundIcon = <div className={styles.ellipse}></div>;
 
   return (
-    <section className={styles.card}>
-      <img
-        className={styles.placeholderPhoto}
-        src={placeholderPhoto}
-        alt="Restaurant Snapshot"
-      />
+    <section
+      className={styles.card}
+      style={{
+        backgroundImage: `url(${restaurantPhoto})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        alt: "Restaurant Snapshot",
+      }}
+    >
       <div className={styles.checkIn}></div>
       <div className={styles.rating}></div>
       <div className={styles.details}>
         <div className={styles.topTextWrapper}>
-          <p className={styles.topText}>salads</p>
+          <p className={styles.topText}>{cardTopTextCategory1}</p>
           {roundIcon}
-          <p className={styles.topText}>snacks</p>
+          <p className={styles.topText}>{cardTopTextCategory2}</p>
           {roundIcon}
-          <p className={styles.topText}>pizza</p>
+          <p className={styles.topText}>{cardTopTextCategory3}</p>
         </div>
-        <h3 className={styles.title}>Ristorante Viva Piccola Italia</h3>
-        <p className={styles.bottomText}>10:00 - 21:00</p>
+        <h3 className={styles.title}>{cardTitle}</h3>
+        <p className={styles.bottomText}>
+          {cardOpenTime} - {cardClosingTime}
+        </p>
       </div>
       <div className={styles.wishlist}></div>
     </section>
   );
 };
+
+RestaurantCard.defaultProps = {
+  cardTopTextCategory1: "category1",
+  cardTopTextCategory2: "category2",
+  cardTopTextCategory3: "category3",
+  cardTitle: "Restaurant title",
+  cardOpenTime: "00:00",
+  cardClosingTime: "00:00",
+};
+
+RestaurantCard.propTypes = {
+  restaurantPhoto: PropTypes.any,
+  cardTopTextCategory1: PropTypes.string,
+  cardTopTextCategory2: PropTypes.string,
+  cardTopTextCategory3: PropTypes.string,
+  cardTitle: PropTypes.string,
+  cardOpenTime: PropTypes.string,
+  cardClosingTime: PropTypes.string,
+};
+
+export default RestaurantCard;
--- src/components/EatOutSection/RestaurantCard/RestaurantCard.stories.jsx
+++ src/components/EatOutSection/RestaurantCard/RestaurantCard.stories.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { RestaurantCard } from "./RestaurantCard";
-import { withKnobs } from "@storybook/addon-knobs";
+import RestaurantCard from "./RestaurantCard";
+import { text, withKnobs } from "@storybook/addon-knobs";
+
+import mockPhoto from "assets/restaurant-card-placeholder.jpg";
+
+const photoOptions = {
+  photo1: mockPhoto,
+};
 
 export default {
   title: "Components / RestaurantCard",
@@ -8,4 +14,21 @@ export default {
   component: RestaurantCard,
 };
 
-export const RestaurantCardComponent = () => <RestaurantCard />;
+export const RestaurantCardComponent = () => {
+  return (
+    <RestaurantCard
+      cardTitle={text(
+        "Restaurant title",
+        "Restaurant long title, exceeding the limits of text"
+      )}
+      restaurantPhoto={photoOptions.photo1}
+      cardTopTextCategory1={text("Top category one", "category1")}
+      cardTopTextCategory2={text("Top category two", "category2")}
+      cardTopTextCategory3={text("Top category three", "category3")}
+      cardOpenTime={text("Open time", "00:00")}
+      cardClosingTime={text("Closing time", "00:00")}
+    />
+  );
+};
+
+// const value = select(label, options, defaultValue, groupId);
