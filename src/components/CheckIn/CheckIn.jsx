57ee64124166e234d1b6a39986f45abc96dbfcc0
Marius <marius.budreika09@gmail.com>
2021-11-28 14:33:23 +0200

    PR fixes

--- src/components/CheckIn/CheckIn.jsx
+++ src/components/CheckIn/CheckIn.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
 import SvgIconCheckInPerson from "components/icon/library/IconCheckInPerson";
 
 import styles from "./check-in.module.scss";
 
-export const CheckIn = ({ count = "6" }) => {
+export const CheckIn = ({ count = 6 }) => {
   return (
     <div className={styles.wrapper}>
       <SvgIconCheckInPerson size="1.2rem" />
-      <div className={styles.count}>{count}</div>
+      <div className={cx(styles.count, styles.caption)}>{count}</div>
     </div>
   );
 };
