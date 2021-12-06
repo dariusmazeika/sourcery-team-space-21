60fa6e05306ba9c7b22bd01c93dea3c0291adbeb
Imantas Cesliavicius <imantas.cesliavicius@gmail.com>
2021-12-06 18:56:29 +0200

    removed old BrowseCard files

--- src/components/EatOutSection/BrowseCardComponent/BrowseCard.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BrowseCard } from "./BrowseCard";
-
-export default {
-  title: "Components / BrowseCard",
-  component: BrowseCard,
-};
-
-export const BrowseCardComponent = () => <BrowseCard />;
