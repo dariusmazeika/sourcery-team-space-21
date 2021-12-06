926d1bf8ac8e9ad156d683eb6d911e6945e0999f
Imantas Cesliavicius <imantas.cesliavicius@gmail.com>
2021-12-06 22:06:17 +0200

    some small changes

--- src/components/EatOutSection/BlankCard/BlankCard.jsx
+++ src/components/EatOutSection/BlankCard/BlankCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./blank-card.module.scss";
 import PropTypes from "prop-types";
 
-export const BlankCard = ({ cardContent }) => {
-  return <section className={styles.card}>{cardContent}</section>;
+export const BlankCard = ({ children }) => {
+  return <section className={styles.card}>{children}</section>;
 };
 
 BlankCard.propTypes = {
-  cardContent: PropTypes.node,
+  children: PropTypes.node,
 };
 
 export default BlankCard;
--- src/components/EatOutSection/BrowseCard/BrowseCard.jsx
+++ src/components/EatOutSection/BrowseCard/BrowseCard.jsx
@@ -3,7 +3,11 @@ import BlankCard from "../BlankCard/BlankCard";
 import BrowseCardContent from "./BrowseCardContent";
 
 export const BrowseCard = () => {
-  return <BlankCard cardContent={<BrowseCardContent />}></BlankCard>;
+  return (
+    <BlankCard>
+      <BrowseCardContent />
+    </BlankCard>
+  );
 };
 
 export default BrowseCard;
