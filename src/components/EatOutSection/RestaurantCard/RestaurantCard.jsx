2604795869e7af5117e5a4c1fd569fa1e3e8c6e8
rimkusaurimas <labas@aurimasrimkus.lt>
2021-12-07 23:04:58 +0200

    Mock data, other bug fixes, dynamic content

--- src/components/EatOutSection/RestaurantCard/RestaurantCard.jsx
+++ src/components/EatOutSection/RestaurantCard/RestaurantCard.jsx
@@ -2,64 +2,115 @@ import React from "react";
 import styles from "./restaurant-card.module.scss";
 import PropTypes from "prop-types";
 
-const RestaurantCard = ({
-  restaurantPhoto,
-  cardTopTextCategory1,
-  cardTopTextCategory2,
-  cardTopTextCategory3,
-  cardTitle,
-  cardOpenTime,
-  cardClosingTime,
-}) => {
-  const roundIcon = <div className={styles.ellipse}></div>;
+export const TestSection = () => {
+  const mockedData = [
+    {
+      name: "Grill London (Kaunas, PLC Mega)",
+      id: "120wsdlpx4",
+      slogan: "Best burgers in the town",
+      createdDate: "2019-06-05",
+      description:
+        "„Grill London” is a chain of grill restaurants that visitors love because of its exceptional and responsible take on food quality. All of its steaks are grilled in unique ceramic grills using ecological fuels.",
+      openingHours: [
+        {
+          days: "Monday - Friday",
+          hours: "10:30 - 22:15",
+        },
+        {
+          days: "Saturday - Sunday",
+          hours: "11:00 - 23:00",
+        },
+      ],
+      website: "http://grilllondon.lt",
+      location: {
+        address: "Islandijos pl. 32, Kaunas",
+        coordinates: {
+          lat: 54.94013552838086,
+          lng: 23.891938359698315,
+        },
+      },
+      phone: "+370 627 93122",
+      image:
+        "https://images.unsplash.com/photo-1498579485796-98be3abc076e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+      checkIns: 8,
+      reviews: [
+        {
+          userName: "Luke Skywalker",
+          id: "730tybsl12",
+          comment: "Tasty burgers, but had to wait a long line.",
+          rating: 4,
+        },
+        {
+          userName: "Uncle Roger",
+          id: "10f836s0pw",
+          comment: "Didn't have fried rice, a bit disappointed.",
+          rating: 3.5,
+        },
+        {
+          userName: "Sara Lars",
+          id: "183gstwpl6",
+          comment:
+            "Food didn’t meet my expectations, but it was fine. But the customer experience was flawless!",
+          rating: 5,
+        },
+        {
+          userName: "Wade Owen Watts",
+          id: "183620uer",
+          comment: "Good burgers, good price",
+          rating: 4,
+        },
+      ],
+      categories: ["Burger", "Salads", "Grill"],
+    },
+  ];
 
   return (
-    <section
+    <div>
+      {mockedData.map((item, index) => (
+        <RestaurantCard key={`${index}`} data={item} />
+      ))}
+    </div>
+  );
+};
+
+export const RestaurantCard = ({ data }) => {
+  const { categories, image, name, openingHours } = data;
+
+  return (
+    <div
       className={styles.card}
       style={{
-        backgroundImage: `url(${restaurantPhoto})`,
-        backgroundSize: "contain",
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         alt: "Restaurant Snapshot",
+        backgroundPosition: "center",
       }}
     >
       <div className={styles.checkIn}></div>
       <div className={styles.rating}></div>
-      <div className={styles.details}>
-        <div className={styles.topTextWrapper}>
-          <p className={styles.topText}>{cardTopTextCategory1}</p>
-          {roundIcon}
-          <p className={styles.topText}>{cardTopTextCategory2}</p>
-          {roundIcon}
-          <p className={styles.topText}>{cardTopTextCategory3}</p>
+      <div className={styles.lowerWrapper}>
+        <div className={styles.details}>
+          <div className={styles.topTextWrapper}>
+            {categories.map((category, index) => (
+              <p key={`${index}`} className={styles.topText}>
+                {category}
+              </p>
+            ))}
+          </div>
+          <h3 className={styles.title}>{name}</h3>
+          <p className={styles.bottomText}>{openingHours.days}</p>
         </div>
-        <h3 className={styles.title}>{cardTitle}</h3>
-        <p className={styles.bottomText}>
-          {cardOpenTime} - {cardClosingTime}
-        </p>
+        <div className={styles.wishlist}></div>
       </div>
-      <div className={styles.wishlist}></div>
-    </section>
+    </div>
   );
 };
 
-RestaurantCard.defaultProps = {
-  cardTopTextCategory1: "category1",
-  cardTopTextCategory2: "category2",
-  cardTopTextCategory3: "category3",
-  cardTitle: "Restaurant title",
-  cardOpenTime: "00:00",
-  cardClosingTime: "00:00",
-};
-
 RestaurantCard.propTypes = {
-  restaurantPhoto: PropTypes.any,
-  cardTopTextCategory1: PropTypes.string,
-  cardTopTextCategory2: PropTypes.string,
-  cardTopTextCategory3: PropTypes.string,
-  cardTitle: PropTypes.string,
-  cardOpenTime: PropTypes.string,
-  cardClosingTime: PropTypes.string,
+  data: PropTypes.object,
+  categories: PropTypes.any,
+  category: PropTypes.string,
+  name: PropTypes.string,
+  openingHours: PropTypes.string,
 };
-
-export default RestaurantCard;
--- src/components/EatOutSection/RestaurantCard/RestaurantCard.stories.jsx
+++ src/components/EatOutSection/RestaurantCard/RestaurantCard.stories.jsx
@@ -1,12 +1,67 @@
 import React from "react";
-import RestaurantCard from "./RestaurantCard";
-import { text, withKnobs } from "@storybook/addon-knobs";
+import { RestaurantCard } from "./RestaurantCard";
+import { withKnobs } from "@storybook/addon-knobs";
 
-import mockPhoto from "assets/restaurant-card-placeholder.jpg";
-
-const photoOptions = {
-  photo1: mockPhoto,
-};
+const mockedData = [
+  {
+    name: "Grill London (Kaunas, PLC Mega)",
+    id: "120wsdlpx4",
+    slogan: "Best burgers in the town",
+    createdDate: "2019-06-05",
+    description:
+      "„Grill London” is a chain of grill restaurants that visitors love because of its exceptional and responsible take on food quality. All of its steaks are grilled in unique ceramic grills using ecological fuels.",
+    openingHours: [
+      {
+        days: "Monday - Friday",
+        hours: "10:30 - 22:15",
+      },
+      {
+        days: "Saturday - Sunday",
+        hours: "11:00 - 23:00",
+      },
+    ],
+    website: "http://grilllondon.lt",
+    location: {
+      address: "Islandijos pl. 32, Kaunas",
+      coordinates: {
+        lat: 54.94013552838086,
+        lng: 23.891938359698315,
+      },
+    },
+    phone: "+370 627 93122",
+    image:
+      "https://images.unsplash.com/photo-1498579485796-98be3abc076e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    checkIns: 8,
+    reviews: [
+      {
+        userName: "Luke Skywalker",
+        id: "730tybsl12",
+        comment: "Tasty burgers, but had to wait a long line.",
+        rating: 4,
+      },
+      {
+        userName: "Uncle Roger",
+        id: "10f836s0pw",
+        comment: "Didn't have fried rice, a bit disappointed.",
+        rating: 3.5,
+      },
+      {
+        userName: "Sara Lars",
+        id: "183gstwpl6",
+        comment:
+          "Food didn’t meet my expectations, but it was fine. But the customer experience was flawless!",
+        rating: 5,
+      },
+      {
+        userName: "Wade Owen Watts",
+        id: "183620uer",
+        comment: "Good burgers, good price",
+        rating: 4,
+      },
+    ],
+    categories: ["Burger", "Salads", "Grill"],
+  },
+];
 
 export default {
   title: "Components / RestaurantCard",
@@ -14,19 +69,12 @@ export default {
   component: RestaurantCard,
 };
 
-export const RestaurantCardComponent = () => {
+export const Default = () => {
   return (
-    <RestaurantCard
-      cardTitle={text(
-        "Restaurant title",
-        "Restaurant long title, exceeding the limits of text"
-      )}
-      restaurantPhoto={photoOptions.photo1}
-      cardTopTextCategory1={text("Top category one", "category1")}
-      cardTopTextCategory2={text("Top category two", "category2")}
-      cardTopTextCategory3={text("Top category three", "category3")}
-      cardOpenTime={text("Open time", "00:00")}
-      cardClosingTime={text("Closing time", "00:00")}
-    />
+    <div>
+      {mockedData.map((item, index) => (
+        <RestaurantCard key={`${index}`} data={item} />
+      ))}
+    </div>
   );
 };
