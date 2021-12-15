ee9a80103a3845a45271a1cacc628baa3c4934b3
Aivaras <as.savinis@gmail.com>
2021-12-15 10:16:26 +0200

    Added react-slick packed for eat out slider.

--- src/pages/EatOut/EatOutPage.jsx
+++ src/pages/EatOut/EatOutPage.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAPI } from "features/context";
+import { BreadCrumbs } from "components/Breadcrumbs";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { EatOutHeroSlider } from "components/EatOutSection";
 
 export const EatOutPage = () => {
-  return <div>{}</div>;
+  const [data, getData] = useAPI();
+
+  useEffect(() => {
+    getData("restaurants");
+  }, []); // eslint-disable-line
+
+  const sliderSettings = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    dots: true,
+  };
+
+  return (
+    <div>
+      <BreadCrumbs />
+      <Slider {...sliderSettings}>
+        {data.restaurants?.map((restaurant, index) => (
+          <EatOutHeroSlider key={restaurant.name} restaurant={restaurant} />
+        ))}
+      </Slider>
+    </div>
+  );
 };
