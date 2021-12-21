3091b7bbb07321af5a0675e06ea309c780ed03bb
Darius M <darius.post@gmail.com>
2021-12-21 19:33:43 +0200

    updated starRating

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import styles from "./star-rating.module.scss";
 import { Icon } from "components/iconSprite/Icon";
 
-export const StarRating = ({ rating = "1.0" }) => {
+export const StarRating = ({ rating }) => {
   return (
     <div className={styles.rating}>
       <Icon className={styles.ratingIcon} name="icon-action-star-filled" />
-      <p className={styles.ratingNumber}>{rating}</p>
+      {rating && <p className={styles.ratingNumber}>{rating}</p>}
     </div>
   );
 };
