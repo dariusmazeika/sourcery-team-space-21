5c4575ad5cb52820c37e7e3de47b8d896da5c1e1
Marius <marius.budreika09@gmail.com>
2022-01-03 19:17:54 +0200

    fixed border radius

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./star-rating.module.scss";
 import { Icon } from "components/iconSprite/Icon";
+import cx from "classnames";
 
-export const StarRating = ({ rating }) => {
+export const StarRating = ({ rating, className }) => {
   return (
-    <div className={styles.rating}>
+    <div
+      className={cx(className, {
+        [styles.rating]: true,
+      })}
+    >
       <Icon className={styles.ratingIcon} name="icon-action-star-filled" />
       {rating && <p className={styles.ratingNumber}>{rating}</p>}
     </div>
@@ -14,4 +19,5 @@ export const StarRating = ({ rating }) => {
 
 StarRating.propTypes = {
   rating: PropTypes.number,
+  className: PropTypes.string,
 };
