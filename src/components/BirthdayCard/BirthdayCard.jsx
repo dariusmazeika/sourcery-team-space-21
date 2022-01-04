fb3c73dbee55d920001dff451670beb0d8189835
Marius <marius.budreika09@gmail.com>
2022-01-04 18:33:49 +0200

    initial birthday card

---
+++ src/components/BirthdayCard/BirthdayCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styles from "./birthday-card.module.scss";d
+import PropTypes from "prop-types";
+
+export const BirthdayCard = (data) => {
+  return <div></div>;
+};
+
+BirthdayCard.propTypes = {
+  data: PropTypes.object,
+};
--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -4,26 +4,30 @@ import { HelloWidget } from "components/HelloWidget/HelloWidget";
 import { ReservationsWidget } from "components/reservationsWidget/ReservationsWidget";
 import { EatOutSection } from "components/EatOutSection/EatOutSection/EatOutSection";
 import { useAPI } from "features/context/APIContext";
+import { BirthdayCard } from "components/BirthdayCard/BirthdayCard";
 
 export const DashboardPage = () => {
   const [data, getData] = useAPI();
 
   //TODO: add useEffect dependencies
   useEffect(() => {
-    getData("userData", "restaurants");
+    getData("userData", "restaurants", "stories");
   }, []); // eslint-disable-line
 
   return (
-    <>
-      <section className={styles.dashboardSection}>
-        <HelloWidget />
-      </section>
-      <section className={styles.dashboardSection}>
-        <ReservationsWidget data={{ userData: data.userData }} />
-      </section>
-      <section className={styles.dashboardSection}>
-        <EatOutSection data={data} />
-      </section>
-    </>
+    data && (
+      <>
+        <section className={styles.dashboardSection}>
+          <HelloWidget />
+        </section>
+        <section className={styles.dashboardSection}>
+          <ReservationsWidget data={{ userData: data.userData }} />
+        </section>
+        <section className={styles.dashboardSection}>
+          <EatOutSection data={data} />
+        </section>
+        <BirthdayCard data={data.stories} />
+      </>
+    )
   );
 };
