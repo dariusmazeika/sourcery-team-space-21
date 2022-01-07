9d96dde91b36d43e13be180f14e2fe488db2cb75
Marius <marius.budreika09@gmail.com>
2022-01-07 09:55:31 +0200

    Changed date format for birthday date

--- src/components/BirthdayCard/BirthdayCard.jsx
+++ src/components/BirthdayCard/BirthdayCard.jsx
@@ -6,6 +6,50 @@ import { Icon } from "components/iconSprite/Icon";
 import confettiImage from "assets/birthday-card-confetti.png";
 
 export const BirthdayCard = ({ data }) => {
+  let month = data.birthdayDate.substring(5, 7);
+  let day = data.birthdayDate.substring(8);
+
+  switch (month) {
+    case "1":
+      month = "Jan";
+      break;
+    case "2":
+      month = "Feb";
+      break;
+    case "3":
+      month = "Mar";
+      break;
+    case "4":
+      month = "Apr";
+      break;
+    case "5":
+      month = "May";
+      break;
+    case "6":
+      month = "Jun";
+      break;
+    case "7":
+      month = "Jul";
+      break;
+    case "8":
+      month = "Aug";
+      break;
+    case "9":
+      month = "Sep";
+      break;
+    case "10":
+      month = "Oct";
+      break;
+    case "11":
+      month = "Nov";
+      break;
+    case "12":
+      month = "Dec";
+      break;
+    default:
+      month = "";
+  }
+
   return (
     <BlankCard className={styles.blankCard}>
       <div className={styles.birthdayCard}>
@@ -19,9 +63,12 @@ export const BirthdayCard = ({ data }) => {
         ></div>
         <div className={styles.birthdayCardUserName}>{data.userName}</div>
         <div className={styles.birthdayCardBirthdayDate}>
-          {data.birthdayDate}
+          Celebrated a birthday on&nbsp;
+          <b>
+            {month} {day}
+          </b>
         </div>
-        <div className={styles.birthdayCardWish}>Send a wish</div>
+        <div className={styles.birthdayCardWish}>Send a wish!</div>
         <hr className={styles.birthdayCardLine}></hr>
         <div className={styles.birthdayCardBottom}>
           <Icon name="icon-other-gift" size="large" />
